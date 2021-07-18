67819962904aedd17e119e6a5ceff0cfb25556f9
Aljawhara <jj.hamlan@hotmail.com>
2021-07-18 21:20:39 +0300

    Initial Commit

---
+++ jAlHamlanApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  jAlHamlanApp
+//
+//  Created by Aljawhara Saleh on 08/12/1442 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
